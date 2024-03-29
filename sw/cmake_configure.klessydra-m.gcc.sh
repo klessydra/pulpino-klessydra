6ed1081f0c48b7109f1bd55e2d6073dbb3f6d0bb
Marcello Barbirotta <74311792+Marcello-Barbirotta@users.noreply.github.com>
2024-03-30 00:56:13 +0100

    Update cmake_configure.klessydra-m.gcc.sh
    added dfT13

--- sw/cmake_configure.klessydra-m.gcc.sh
+++ sw/cmake_configure.klessydra-m.gcc.sh
@@ -44,6 +44,7 @@ then
 	USE_KLESSYDRA_OoO=0    # if you are using klessydra-t1-3th (The four pipeline version of klessydra t1), set this to 1CV
 	USE_KLESSYDRA_F0_3TH=0 # if you are using klessydra-f0-3th (The four pipeline version of klessydra f0), set this to 1
 	USE_KLESSYDRA_FT13=0 # if you are using klessydra-fT13 (The temporally fault tolerant version of the T13), then set this to 1
+        USE_KLESSYDRA_dFT13=0 # if you are using klessydra-dfT13 (The dynamic temporally fault tolerant version of the T13), then set this to 1
 	USE_KLESSYDRA_NETLIST=0 # if you are using klessydra-t1-3th (The four pipeline version of klessydra t1), set this to 1
 
 	#  -------------------------------------------------------------------------------------------------------------------------------------------
